aba85cd82ea91e78d9b8039d1ae01627db7b4795
MartinMosqueira <tinchomosqueira@gmail.com>
2023-08-19 08:59:49 -0300

    fix(Project-SI): :bulb: Added comments on enpoints

--- Project-SI/src/main/java/app/project/ProjectSI/controller/MensajeMuroController.java
+++ Project-SI/src/main/java/app/project/ProjectSI/controller/MensajeMuroController.java
@@ -20,26 +20,31 @@ public class MensajeMuroController {
         this.muroService = muroService;
     }
 
+    //NOTE: crear un mensaje en el muro.
     @PostMapping("/create")
     public ResponseEntity<MensajeDTO> create_mensajeMuro_controller(@Valid @RequestBody MensajeMuro mensajeMuro){
         return ResponseEntity.ok(muroService.create_mensajeMuro_service(mensajeMuro));
     }
 
+    //NOTE: modificar mi mensaje en el muro.
     @PutMapping("/update/{id}")
     public ResponseEntity<MensajeDTO> update_mensajeMuro_controller(@PathVariable Long id, @Valid @RequestBody MensajeMuro mensajeMuro){
         return ResponseEntity.ok(muroService.update_mensajeMuro_service(id, mensajeMuro));
     }
 
+    //NOTE: eliminar mi mensaje en el muro.
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> delete_mensajeMuro_controller(@PathVariable Long id){
         return ResponseEntity.ok(muroService.delete_mensajeMuro_service(id));
     }
 
+    //NOTE: obtener mis mensaje publicados en el muro.
     @GetMapping("/get/")
     public ResponseEntity<Set<MensajeDTO>> get_mensajeMuro_controller(){
         return ResponseEntity.ok(muroService.get_MensajesMuro_service());
     }
 
+    //NOTE: obtener todos los mensajes publicados en el muro de todos los usuarios a los que sigo y los mios.
     @GetMapping("/get/all")
     public ResponseEntity<Set<MensajeDTO>> get_all_mensajeMuro_controller(){
         return ResponseEntity.ok(muroService.get_all_MensajeMuro_service());
--- Project-SI/src/main/java/app/project/ProjectSI/controller/UsuarioController.java
+++ Project-SI/src/main/java/app/project/ProjectSI/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import app.project.ProjectSI.request.UsuarioDTO;
 import app.project.ProjectSI.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -15,29 +14,30 @@ import java.util.Set;
 public class UsuarioController {
 
     @Autowired
-    private SessionRegistry sessionRegistry;
-
-    @Autowired
     private UsuarioService usuarioService;
 
+    //NOTE: obtener usuario logueado.
     @GetMapping("/get/")
     public ResponseEntity<UsuarioDTO> get_usuario_controller() {
         UsuarioDTO usuario = usuarioService.get_usuario_service();
         return ResponseEntity.ok(usuario);
     }
 
+    //NOTE: buscar usuario por username.
     @PostMapping("/find/{username}")
     public ResponseEntity<UsuarioDTO> find_username_controller(@PathVariable String username) {
         UsuarioDTO nameUsuario = usuarioService.find_usuario_service(username);
         return ResponseEntity.ok(nameUsuario);
     }
 
+    //NOTE: crear usuario.
     @PostMapping("/create")
     public ResponseEntity<Usuario> create_usuario_controller(@RequestBody Usuario usuario) {
         Usuario newUsuario = usuarioService.create_usuario_service(usuario);
         return ResponseEntity.ok(newUsuario);
     }
 
+    //NOTE: actualizar usuario.
     @PutMapping ("/update")
     public ResponseEntity<UsuarioDTO> update_usuario_controller(@RequestBody Usuario usuario) {
         UsuarioDTO updateUsuario = usuarioService.update_usuario_service(usuario);
@@ -46,18 +46,21 @@ public class UsuarioController {
 
     //CONTACTOS ENPOINTS
 
+    //NOTE: agregar contacto.
     @PostMapping("/add/contacto/{contactoID}")
     public ResponseEntity<String> add_contacto_controller(@PathVariable Long contactoID) {
         String contacto = usuarioService.add_contacto_service(contactoID);
         return ResponseEntity.ok(contacto);
     }
 
+    //NOTE: obtener todos los contactos de un usuario.
     @GetMapping("/get/contactos")
     public ResponseEntity<Set<UsuarioDTO>> get_contactos_controller() {
         Set<UsuarioDTO> contactos = usuarioService.get_all_contactos_service();
         return ResponseEntity.ok(contactos);
     }
 
+    //NOTE: eliminar un contacto.
     @DeleteMapping("/delete/contacto/{contactoID}")
     public ResponseEntity<String> delete_contacto_controller(@PathVariable Long contactoID) {
         String deleteContacto = usuarioService.delete_contacto_service(contactoID);
@@ -66,18 +69,21 @@ public class UsuarioController {
 
     //SESSIONS ENDPOINTS
 
+    //NOTE: seguir a un usuario.
     @PostMapping("/add/seguido/{seguidoID}")
     public ResponseEntity<String> add_seguido_controller(@PathVariable Long seguidoID) {
         String seguido = usuarioService.add_seguido_service(seguidoID);
         return ResponseEntity.ok(seguido);
     }
 
+    //NOTE: obtener todos los usuarios seguidos de un usuario.
     @GetMapping("/get/seguidos")
     public ResponseEntity<Set<UsuarioDTO>> get_seguidos_controller() {
         Set<UsuarioDTO> seguidos = usuarioService.get_all_seguidos_service();
         return ResponseEntity.ok(seguidos);
     }
 
+    //NOTE: dejar de seguir a un usuario.
     @DeleteMapping("/delete/seguido/{seguidoID}")
     public ResponseEntity<String> delete_seguido_controller(@PathVariable Long seguidoID) {
         String deleteSeguido = usuarioService.delete_seguido_service(seguidoID);

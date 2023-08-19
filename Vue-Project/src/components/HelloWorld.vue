e3ed64c0d6f0233a091348d8b61e1ada6960b1e9
MartinMosqueira <tinchomosqueira@gmail.com>
2023-08-19 12:06:16 -0300

    fix(Vue-Project): :tada: Start project Vue

---
+++ Vue-Project/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h3>
+      Proyecto de microblogging para Ingenieria de Software.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Vue-Project/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>This is an about page</p>
+  </div>
+</template>
